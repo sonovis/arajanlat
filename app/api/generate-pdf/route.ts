e11817c12ae67e49be0b86890b76f40a678efe22
Benedek Dévényi <it@sonovis.hu>
2026-04-21 19:59:15 +0200

    hupszi

--- app/api/generate-pdf/route.ts
+++ app/api/generate-pdf/route.ts
@@ -44,7 +44,7 @@ export async function POST(req: Request) {
       [
         "compile",
         "--font-path",
-        fontDir,
+        dstFontsDir,
         inputPath,
         outputPath,
       ],
